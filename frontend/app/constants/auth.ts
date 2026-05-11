3ae1613dcd8f775fecca96aa4123f2c1eeb800b6
Alan Kardek Filho <alankardekfilho@hotmail.com>
2026-05-11 16:32:26 -0300

    Update auth.ts

--- frontend/app/constants/auth.ts
+++ frontend/app/constants/auth.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://credenciamento-de-eventos.onrender.com";
 export const TOKEN_STORAGE_KEY = "eventos_token";
 
